cdfc226c766ec54732e960e2dbc2bccf82c0aed2
officialmuneebahmad <officialmuneebahmad@gmail.com>
2025-06-04 14:18:36 +0500

    server file fixed

--- server.js
+++ server.js
@@ -47,5 +47,9 @@ app.post('/api/questions', async (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+app.get('/', (req, res) => {
+  res.send('MCQs Backend is running!');
+});
+
 
 // kzone710 emersonunimul
